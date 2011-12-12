26839eea0e9ddc11a8775bfaf8ecb6577739690e
Florentin Zorca <f@zorca.de>
2011-12-12 11:25:24 +0100

    updated the example and added examples into the readme file

--- example.js
+++ example.js
@@ -1,23 +1,25 @@
-var ringBuffer = require('./ringBuffer');
+// first, require ringbuffer
+var ringBuffer = require('ringbuffer');
 
+// create a tiny buffer of size 3
 var buffer = ringBuffer.ringBuffer(3);
-var i = 1;
+var i;
 
 // reading from empty buffer yields 'undefined', no error
 console.log(buffer.read());
 
 // write and read from buffer
 buffer.write('it works!');
+// note: reading 'consumes' (dereferences) the item from buffer
 console.log(buffer.read());
 
 // write 5 times in the buffer of size 3
 for(i=1; i<=5; i++){
-	buffer.write(i);
+    buffer.write(i);
 }
 
-// read 5 times form the buffer of size 3 (while no one write in it)
+// read 5 times from the buffer of size 3 (while no one write in it)
 // will just return the last three written elements
 for(i=1; i<=5; i++){
-	console.log(buffer.read());
+    console.log(buffer.read());
 }
-
